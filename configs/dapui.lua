d299704535e351623a2f0eeda4d65e1b2083ea7c
Josh Berlin <joshua.berlin@sense.com>
2023-07-10 16:50:33 -0400

    add rust debug 1st attempt

--- configs/dapui.lua
+++ configs/dapui.lua
@@ -8,3 +8,26 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
+
+require('dap.ext.vscode').load_launchjs('launch.json', {})
+
+local mason_registry = require("mason-registry")
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+
+dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+
+dap.configurations.rust = {
+  {
+    name = "Rust debug",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', '${workspaceFolder}' .. '/target/debug/', 'file')
+    end,
+    -- cwd = '${workspaceFolder}',
+    -- stopOnEntry = true,
+    showDisassemply = "never"
+  },
+}
